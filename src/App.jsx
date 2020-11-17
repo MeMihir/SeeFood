a705c1e7f81c8ffeb47f72e50c0236a52652f3ff
Mihir <pavuskarmihir@gmail.com>
2020-11-17 22:09:33 +0530

    basic setup done.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+import Background from './components/Background';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="area">
+        <header className="App-header">
+        SeeFood
+        </header>
+        <Background/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
